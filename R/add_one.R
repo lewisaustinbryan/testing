f27ad90c9dc3b1669c270cafae2e696e0adf7637
lewisaustinbryan <lewis.a.bryan@googlemail.com>
2021-03-12 17:30:11 +0000

    Creating R package

--- R/add_one.R
+++ R/add_one.R
@@ -1,3 +1,14 @@
+#' add 1 to a number
+#'
+#' This function takes any numerical input and adds 1 to it.
+#' @param number any number
+#' @return number added by one.
+#' @export
+#' @examples
+#' add_one(5)
+
+
+
 add_one <- function(number) {
   return(number + 1)
 }
